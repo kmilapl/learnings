7dc08b9d4b19504d9d88a3a4a9bdc716ed9aef50
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-05-30 15:15:44 -0300

    Add files via upload

---
+++ java/operadores.java
@@ -0,0 +1,42 @@
+package domain;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        //Operador de SOMA
+        
+        int valor1 = 10;
+        int valor2 = 10;
+        int soma = valor1 + valor2;
+        
+        System.out.println(soma);
+        
+        //Operador de SUBTRAÇÃO
+        
+        int valor3 = 20;
+        int valor4 = 10;
+        int sub = valor3 - valor4;
+        
+        System.out.println(sub);
+        
+        //Operador de MULTIPLICAÇÃO
+        
+        int valor5 = 2;
+        int valor6 = 2;
+        int mult = valor5 * valor6;
+        
+        System.out.println(mult);
+        
+        //Operador de DIVISÃO
+        
+        int valor7 = 4;
+        int valor8 = 2;
+        int div = valor7 / valor8;
+        
+        System.out.println(div);
+        
+    }
+
+}

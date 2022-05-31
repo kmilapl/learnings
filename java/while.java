3a70464173bbd2e57a83bfb7f6cea63959fe39fe
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-05-30 22:48:32 -0300

    Add files via upload

---
+++ java/while.java
@@ -0,0 +1,18 @@
+package domain;
+//while = enquanto (repete os dados conforme a lógica passada)
+//incrementação = vai adicionando o valor informado até o maximo do valor passado na condição
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        int contador = 0;
+        
+        while(contador <= 5){
+            System.out.println(contador);
+            contador += 1; //incrementação
+        }
+
+    }
+
+}

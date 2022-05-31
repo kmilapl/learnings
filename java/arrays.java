98fa6be99fbd3f023ad62cdab219ede9551e605f
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-05-30 23:02:14 -0300

    Add files via upload

---
+++ java/arrays.java
@@ -0,0 +1,17 @@
+package domain;
+//arrays = lista
+//indices de um array começa do 0, e não do 1
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        String[] lista_nome = {"Camila", "Natalia", "Amanda", "Maria", "Joana", "Victor"};
+        System.out.println(lista_nome[0]);
+        
+        int[] lista_idade = {23, 21, 19, 54, 75, 23};
+        System.out.println(lista_idade[0]);
+
+    }
+
+}

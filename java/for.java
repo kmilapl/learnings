6a7c29714470f278fa428f54b889e8476186eac7
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-05-30 22:52:44 -0300

    Add files via upload

---
+++ java/for.java
@@ -0,0 +1,22 @@
+package domain;
+//for = faz basicamente o mesmo que o while, porém de uma forma simplificada.
+//
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        /*
+        int contador = 0;
+        while(contador <= 5){
+            System.out.println(contador);
+            contador += 1; //incrementação
+        }*/
+        
+        for(int contador = 0; contador <=5; contador ++){
+            System.out.println(contador);
+        }
+
+    }
+
+}

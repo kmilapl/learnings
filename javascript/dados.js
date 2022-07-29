2b3c805d335843bd33524abd57e0296459735ecb
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-07-29 10:51:54 -0300

    Add files via upload

---
+++ javascript/dados.js
@@ -0,0 +1,22 @@
+/* Valores Literais 
+console.log("Caneta");
+console.log(10); 
+console.log(6.4);
+console.log(1.5);
+console.log(7.9);
+*/
+
+var nome = "Caneta";
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco+imposto;
+
+console.log(nome);
+console.log(quantidade); 
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+let nome = "Caneta BIC";
+console.log(nome);

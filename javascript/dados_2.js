fdd5a3a7c79b8f4b45ee68d9109bc80a0b4a0255
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-07-29 10:58:23 -0300

    Add files via upload

---
+++ javascript/dados_2.js
@@ -0,0 +1,16 @@
+//Javascript não diferencia números inteiros de quebrados. Tudo é number, sendo int ou float
+//Number
+let idade = 23;
+console.log(typeof 23);
+console.log(typeof idade);
+
+let salario = 4568.32;
+console.log(typeof salario);
+
+//Boolean
+let estaChovendo = false; //true ou false
+console.log(typeof estaChovendo);
+
+//String
+let nome = "Camila";
+console.log(typeof nome);

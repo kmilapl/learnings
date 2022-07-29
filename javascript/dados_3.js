4414744d9293a418451cc0e47ef9a236483aee77
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-07-29 11:04:08 -0300

    Add files via upload

---
+++ javascript/dados_3.js
@@ -0,0 +1,11 @@
+var a = 3;
+
+//Modificar valor da variável
+
+var a = 10;
+
+console.log(a);
+
+//Const = variavel passa a ser constante, e não poderá ser mais modificada
+const b = 3;
+console.log(b);

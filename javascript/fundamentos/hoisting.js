c9e1260eb29b792a9ba4bcd056f37bdfaa86c4c1
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-08-03 15:42:18 -0300

    Add files via upload

---
+++ javascript/fundamentos/hoisting.js
@@ -0,0 +1,13 @@
+console.log('a = ', a)
+var a = 2
+console.log('a =', a)
+//ou
+function teste() {
+    console.log('a =', a)
+    var a = 2
+    console.log('a =', a)
+}
+
+console.log('b = ', b)
+var b = 2
+console.log('b =', b)

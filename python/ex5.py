3ee8926279a450f34f36a528549fe36cf005f11b
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-08-08 10:43:44 -0300

    Add files via upload

---
+++ python/ex5.py
@@ -0,0 +1,7 @@
+#Faça um Programa que converta metros para centímetros.
+
+print("Vamos converter o metro em centímetro?")
+m = int(input("Digite o metro: "))
+cm = m*100
+
+print("Convertido em centímetros: ", cm)

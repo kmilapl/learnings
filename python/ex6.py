b400c72aec740acfd31562deb9d566122477319a
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-08-08 10:54:07 -0300

    Add files via upload

---
+++ python/ex6.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio = int(input("Digite um número: "))
+pi = 3.14
+area = (pi * raio * raio)
+
+print("O resultado da área é: ", area,"m²")

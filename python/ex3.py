07a3697c28977b1a6b73e462d9ab4743790575a1
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-08-08 10:39:18 -0300

    Add files via upload

---
+++ python/ex3.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+print("Vamos somar?")
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+print("A soma é:", num1 + num2)
---
+++ python/ex4.py
@@ -0,0 +1,10 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+print("Vamos calcular a média das 4 notas bimestrais?")
+bim = 4
+nota1 = float(input("Insira a primeira nota: "))
+nota2 = float(input("Insira a segunda nota: "))
+nota3 = float(input("Insira a terceira nota: "))
+nota4 = float(input("Insira a última nota: "))
+
+print("A média dessas notas é: ", (nota1+nota2+nota3+nota4)/bim)

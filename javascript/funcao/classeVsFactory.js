1cf83f97194c74dded1e3fe6d6b24b3f09d77ada
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-08-16 12:12:05 -0300

    Add files via upload

---
+++ javascript/funcao/classeVsFactory.js
@@ -0,0 +1,24 @@
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    falar() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('Camila')
+p1.falar()
+
+const criarPessoa = nome => {
+    return {
+        falar: () => console.log(`Meu nome é ${nome}`)
+    }
+}
+
+const p2 = criarPessoa('Natalia')
+p2.falar()
+
+const p3 = criarPessoa('Amanda')
+p3.falar()

9556c4106072de71b58e34a44538e79754060873
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-08-16 12:17:15 -0300

    Add files via upload

---
+++ javascript/funcao/iife.js
@@ -0,0 +1,9 @@
+//IIFE - Immediately Invoked Function Expression
+//Imediatamente invocada
+//Fugir do escopo global
+
+(function() {
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})()
+

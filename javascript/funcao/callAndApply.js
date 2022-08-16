8c4a0412a8eec290449786a4b9bca482c238f4f2
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-08-16 12:24:06 -0300

    Add files via upload

---
+++ javascript/funcao/callAndApply.js
@@ -0,0 +1,26 @@
+//Call
+//Apply
+//A diferença entre os dois é exatamente a forma como você passa os parâmetros na hora de invocar a função.
+
+function getPreco(imposto = 0, moeda = 'R$'){
+    return `${moeda} ${this.preco * (1 - this.desc) * (1 + imposto)}`
+}
+
+const produto = {
+    nome: 'Notebook',
+    preco: 4589,
+    desc: 0.15,
+    getPreco
+}
+
+global.preco = 20
+global.desc = 0.1
+console.log(getPreco())
+console.log(produto.getPreco())
+
+const carro = {preco: 49990, desc: 0.20}
+console.log(getPreco.call(carro))
+console.log(getPreco.apply(carro))
+
+console.log(getPreco.call(carro, 0.17, '$'))
+console.log(getPreco.apply(global, [0.17, '$'])) //os parametros precisam estar dentro de um array no metodo apply

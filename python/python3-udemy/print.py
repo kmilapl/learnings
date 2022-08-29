a3a2aa936b9e11f3d9ea3bfcfc789bb4f503bdda
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-08-29 12:18:56 -0300

    Add files via upload

---
+++ python/python3-udemy/print.py
@@ -0,0 +1,15 @@
+print(123456)
+print('Luiz', 'Otavio')
+print('Luiz', 'Otavio', sep='-')
+#print('João', 'e', 'Maria', sep='-', end='<3')
+
+
+
+#Exercicio1
+"""
+Transformar CPF no formato abaixo:
+824.176.070-18
+"""
+
+print('824', '176', '070', sep='.', end='-')
+print('18')
---
+++ python/python3-udemy/teste1.py
@@ -0,0 +1,5 @@
+"""
+print('Hello world!')
+"""
+# print("Olá mundo!")
+print('FIM')

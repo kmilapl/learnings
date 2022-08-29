3e003406a5639bd13fe23ecfbeb90b86019804ea
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-08-29 12:34:53 -0300

    Add files via upload

---
+++ python/python3-udemy/dadosPrimitivos.py
@@ -0,0 +1,12 @@
+"""
+Tipos de dados
+str - string
+int - inteiro
+float - real/ponto flutuante
+bool - booleano/lógico
+"""
+
+print('Luiz', type('Luiz'))
+print(10, type(10))
+print(25.23, type(25.23))
+print('l' == 'L', type('l' == 'L'))

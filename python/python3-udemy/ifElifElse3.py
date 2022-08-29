249a1d6960415fe41f5b1b9389816992ed24b7c5
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-08-29 17:13:31 -0300

    Add files via upload

---
+++ python/python3-udemy/ifElifElse3.py
@@ -0,0 +1,21 @@
+a = 2
+b = 3
+if not b > a:
+    print('B é maior do que A')
+else:
+    print('A é maior do que B')
+
+
+
+c = ''
+d = 0
+if not c:
+    print('Por favor, preencha o valor de C.')
+
+
+
+nome = 'Camila'
+if 'a' in nome:
+    print("Existe a letra A no seu nome.")
+else:
+    print("Não existe")

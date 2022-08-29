05a9d43daea42948075d3cb5b621314fb2faf74c
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-08-29 17:18:15 -0300

    Add files via upload

---
+++ python/python3-udemy/teste3.py
@@ -0,0 +1,12 @@
+if False:
+    print('Um')
+elif False:
+    print('Dois')
+elif False:
+    print('Três')
+elif True:
+    print('Quatro')
+elif False:
+    print('Cinco')
+else:
+    print('Seis')

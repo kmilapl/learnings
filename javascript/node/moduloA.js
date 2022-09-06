91b9f62870cb99a23271e8b13cc77f0e69650925
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-09-06 10:56:02 -0300

    Add files via upload

---
+++ javascript/node/moduloA.js
@@ -0,0 +1,4 @@
+// formas de exportar pra fora do node
+this.ola = 'Fala Pessoal'
+exports.bemVindo = 'Bem vindo ao node!'
+module.exports.ateLogo = 'Até proximo exemplo'
---
+++ javascript/node/moduloB.js
@@ -0,0 +1,7 @@
+// mais uma forma de exportar pra fora do node
+module.exports = {
+    bomDia: 'Bom dia',
+    boaNoite() {
+        return 'Boa noite'
+    }
+}

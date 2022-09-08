39966e13a4a39d15989c914319e9e8e1984e787f
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2022-09-08 20:38:55 -0300

    Create ex1.py

---
+++ python/estrutura-de-dados-FAM/ex1.py
@@ -0,0 +1,12 @@
+#troca de valores das variáveis na memória
+#utiliza uma variável auxiliar
+
+x = 5
+y = 10
+
+temp = x
+x = y
+y = temp
+
+print('Valor de X depois: ', x)
+print('Valor de Y depois: ', y)

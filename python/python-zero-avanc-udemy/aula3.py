4c6d15d501e71e1e61e4ffca26191f706beff155
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2023-03-29 13:21:08 -0300

    Add files via upload

---
+++ python/python-zero-avanc-udemy/aula3.py
@@ -0,0 +1,8 @@
+#print(nome, 'tem', altura, 'de altura, pesa', peso, 'quilos e seu IMC é', imc)
+nome = 'Camila Lira'
+altura = 1.50
+peso = 48
+imc = peso / (altura * altura)
+
+linha = f'{nome} tem {altura} de altura pesa {peso} quilos e seu IMC é {imc}'
+print(linha)

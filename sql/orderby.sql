266d09fed7ddcd4bed5eecc2a3eaf6f528f066b7
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2023-05-26 13:09:47 -0300

    Add files via upload

---
+++ sql/orderby.sql
@@ -0,0 +1,11 @@
+--ordena seleção de acordo com a regra definida pelo usuário
+--order by é ordem crescente, pra ordernar decrescente usar desc
+select *
+from sales.products
+order by price desc
+
+select distinct state
+from sales.customers
+order by state
+
+

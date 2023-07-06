9f9b0af4fe0e47d135260a9d2498ccea7b2aa796
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2023-07-06 12:24:30 -0300

    Add files via upload

---
+++ sql/53ordenandoClientesMAIOR150.sql
@@ -0,0 +1,12 @@
+SELECT
+	cus.customer_id,
+    cus.first_name as nome,
+    cus.last_name as sobrenome,
+    sum(amount) as total
+    
+FROM payment pay
+JOIN customer cus USING(customer_id)
+
+GROUP BY customer_id
+HAVING total >= 150
+ORDER BY total DESC

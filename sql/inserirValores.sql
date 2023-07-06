ad0530f6a15a4c2d0494ccad2ff0b67b50c49b08
Camila Lira <88741850+kmilapl@users.noreply.github.com>
2023-07-06 16:18:33 -0300

    Add files via upload

---
+++ sql/inserirValores.sql
@@ -0,0 +1,11 @@
+INSERT INTO marca(nome_marca, origem)
+VALUES ('BMW', 'Alemanha'),
+	('Fiat', 'Italia'),
+    ('Mercedes-Benz', 'Alemanha'),
+    ('Renault', 'Franca'),
+    ('Jaguar', 'Inglaterra');
+
+INSERT INTO inventario(modelo, transmissao, motor, tipo_combustivel, marcas_id)
+VALUES ('BMW 218', 'Automatica', '2.0', 'Gasolina', '1'),
+	('XE 2.0D', 'Manual', '2.0', 'Diesel', '5');
+    
